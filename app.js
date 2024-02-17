ca2dc975af875b3161225713a395f82936f6fc27
smendest <smendestello@gmail.com>
2024-02-16 22:21:26 -0300

    feature: import constants through modules

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { STRINGS } from "./src/constants.js";
+
 // Assign titles and strings
 document.getElementById('page-title').innerText = STRINGS.TITLE;
 document.getElementById('title').innerText = STRINGS.TITLE
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-const STRINGS = {
+export const STRINGS = {
     TITLE: "Encriptador",
   }
