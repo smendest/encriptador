8b99fc4113a8bc4d4f37be2c549a29d88b830fe6
smendest <smendestello@gmail.com>
2024-02-16 21:01:35 -0300

    add basic project structure

---
+++ app.js
@@ -0,0 +1,7 @@
+const STRINGS = {
+  TITLE: "Encriptador",
+}
+
+// Assign titles and strings
+document.getElementById('page-title').innerText = STRINGS.TITLE;
+document.getElementById('title').innerText = STRINGS.TITLE
