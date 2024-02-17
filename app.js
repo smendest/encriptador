ec011ce405954bc4e98083741512caa0a5581761
smendest <smendestello@gmail.com>
2024-02-17 17:09:26 -0300

    create basic input-output template

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ import { STRINGS } from "./src/constants.js";
 // Assign titles and strings
 document.getElementById('page-title').innerText = STRINGS.TITLE;
 document.getElementById('title').innerText = STRINGS.TITLE
+document.getElementById('text-area-label').innerText = STRINGS.TEXT_AREA;
+
+const textArea = document.getElementById('text-area');
+textArea.placeholder = STRINGS.TEXT_AREA_PLACEHOLDER;
+
+document.getElementById('output-paragraph-label').innerHTML = STRINGS.OUTPUT_LABEL;
+
+const output = document.getElementById('output-paragraph');
+
+// TODO: Get the input content and create the algotithm to encrypt and decrypt
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
 export const STRINGS = {
-    TITLE: "Encriptador",
+  TITLE: "Encriptador",
+  TEXT_AREA: "Ingrese el texto que desea encriptar o desencriptar",
+  TEXT_AREA_PLACEHOLDER: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis expedita molestiae non dicta illum voluptate tenetur quo, ullam numquam ipsa perspiciatis libero suscipit totam pariatur doloremque delectus commodi vel nisi.",
+  OUTPUT_LABEL: "Resultado:",
   }
